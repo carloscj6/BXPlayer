fe8d90ba0f409b2f86a129023dcb7decb6ee10f9
carlos <anyona6.cj@gmail.com>
2019-02-08 03:44:34 +0300

    code cleanup

--- app/src/main/java/com/revosleap/bxplayer/models/events/ArtistInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.revosleap.bxplayer.models.events
-
-import com.revosleap.bxplayer.models.Album
-import com.revosleap.bxplayer.models.Song
-
-class ArtistInfo {
-    var albums: MutableList<Album>? = null
-    var songs: MutableList<Song>? = null
-}
